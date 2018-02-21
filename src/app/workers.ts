e539e279bf4ac31b25407369353188c8560c6546
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-02-20 23:50:10 -0600

    curriculum update

---
+++ src/app/workers.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs/operators';
+
+const API_URL = 'https://api.angularbootcamp.com';
+
+@Injectable()
+export class Workers {
+  constructor(private http: HttpClient) { }
+
+  workerList() {
+    const delayUntil: Date = new Date(Date.now() + 1600);
+    return this.http.get(API_URL + '/employees')
+      .pipe(delay(delayUntil));
+  }
+}
